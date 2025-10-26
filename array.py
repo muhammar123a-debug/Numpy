15f6b940e9f7c0f2cef0e32309039cdcb0ec4fd7
muhammar123a-debug <muhammar123a@gmail.com>
2025-10-26 20:51:45 +0500

    array file created

--- array.py
+++ array.py
@@ -3,3 +3,12 @@ import numpy as np
 
 arr_2d = np.array([[22,43,55],[65,76,67]])
 print(arr_2d.shape)
+
+#Size total number of elements in array
+print(arr_2d.size)
+
+#Number of dimensions
+print(arr_2d.ndim)
+
+#data type of elements in array
+print(arr_2d.dtype)
