8bbe358e762907c478008500bd3cd2e0d6b27d46
muhammar123a-debug <muhammar123a@gmail.com>
2025-10-26 20:21:27 +0500

    array file created

--- 1.py
+++ 1.py
@@ -1,1 +1,7 @@
 import numpy as np 
+# One dimentional array
+arr_1d = np.array([22, 33, 44, 55, 66])
+print(arr_1d)
+
+arr_2d = np.array([[43, 54, 65],[34, 21,33]])
+print(arr_2d)
---
+++ array.py
@@ -0,0 +1,5 @@
+#Shape in Arrays 
+import numpy as np
+
+arr_2d = np.array([[22,43,55],[65,76,67]])
+print(arr_2d.shape)
