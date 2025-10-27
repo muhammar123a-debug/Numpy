b5f23a72700878a1e38d5309fd3c0a69ddbf4b74
muhammar123a-debug <muhammar123a@gmail.com>
2025-10-27 11:19:26 +0500

    array code add

--- array.py
+++ array.py
@@ -12,3 +12,27 @@ print(arr_2d.ndim)
 
 #data type of elements in array
 print(arr_2d.dtype)
+
+#astype 
+arr = np.array([1.2, 3.2, 2.4])
+print(arr.dtype)
+int_arr = arr.astype(int)
+print(int_arr)
+print(int_arr.dtype)
+
+#operators in numpy
+arr1 = np.array([1,2,3])
+print(arr1 + 2)  # Addition
+print(arr1 * 2)  # Multiplication
+print(arr1 ** 2)  # Exponentiation
+print(arr1 - 2)  # Subtraction
+print(arr1 / 2)  # Division
+print(arr1 % 2)  # Modulus
+
+# aggregation functions
+# Sum Function
+print(np.sum(arr1))  # Sum of all elements
+print(np.min(arr1))  # Sum of all elements
+print(np.max(arr1))  # Sum of all elements
+print(np.mean(arr1))  # Sum of all elements
+print(np.std(arr1))  # Sum of all elements
