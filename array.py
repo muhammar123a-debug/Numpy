53fde3c48cbf5ba8c2f10afb6ac4d49a21f45c30
muhammar123a-debug <muhammar123a@gmail.com>
2025-10-28 14:29:04 +0500

    array code add

--- array.py
+++ array.py
@@ -36,3 +36,35 @@ print(np.min(arr1))  # Sum of all elements
 print(np.max(arr1))  # Sum of all elements
 print(np.mean(arr1))  # Sum of all elements
 print(np.std(arr1))  # Sum of all elements
+
+
+#Indexing In array
+arr3 = np.array([10,30,40,50,60])
+print(arr3[0])
+print(arr3[2])
+print(arr3[-2])
+
+#Slicing in array
+arr4 = np.array([10,20,30,40,50,60,70,80])
+#[Start:End:Step]
+print(arr4[1:5])
+print(arr4[:3])
+print(arr4[3:])
+print(arr4[::2])
+print(arr4[::-2])
+
+
+#Fency indexing 
+arr5 = np.array([10,20,30,40,50,60,70,80])
+arr5 = arr5[[1, 3, 5]]
+print(arr5)
+
+#Filtering in array
+arr6 = np.array([10,25,30,45,50,65,70,85])
+filtering = arr6 > 34
+print(arr6[filtering])
+
+# reshaping and manipulating arrays
+arr7 = np.array([10,25,30,45,50,65,70,85])
+reshaped_arr = arr7.reshape(2,4)
+print(reshaped_arr)
