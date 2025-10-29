50658d12ee19be3c5f56d128a170cf0ee21686a7
muhammar123a-debug <muhammar123a@gmail.com>
2025-10-29 12:43:30 +0500

    add broadcasting files

--- BroadCasting/broadcasting.py
+++ BroadCasting/broadcasting.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+arr1 = np.array([1, 2, 3])
+result = arr1 * 2
+print(result) 
+
+
+# 1d to 2d array broadcasting
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+vector = np.array([10,20,30])
+result = arr2 + vector
+print(result)
