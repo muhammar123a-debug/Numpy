5345df65142a977f98687a37705f2af02746dfca
muhammar123a-debug <muhammar123a@gmail.com>
2025-11-01 19:29:17 +0500

    add datasets and start reall world practice

--- practice/shopingdata.py
+++ practice/shopingdata.py
@@ -2,3 +2,14 @@ import numpy as np
 
 data = np.genfromtxt("shopping_behavior_updated.csv", delimiter=",", skip_header=1)
 print(data)
+print(data.shape) #Shape is (3900, 18)
+print(data.ndim) #dimention is (2)
+print(data.dtype) #dtype is float
+print(data[:5]) #print 5 rows 
+rows, col = data.shape
+print("Total Rows : ", rows)
+print("Total Column : ", col)
+print(data.min())
+print(data.max())
+print(data.mean())
+print(np.isnan(data).count())
